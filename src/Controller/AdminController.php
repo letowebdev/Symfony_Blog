3ca44572963918d2f2650f7dbe35e33bd380a661
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-05-29 06:42:38 +0100

    commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_main_page")
+     */
+    public function index()
+    {
+        return $this->render('admin/my_profile.html.twig');
+    }
+}
