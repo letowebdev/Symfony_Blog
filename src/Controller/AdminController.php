31457411db411a61d8c48c4c3beddd81f1388576
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-05-29 07:47:11 +0100

    commit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,4 +18,44 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/my_profile.html.twig');
     }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories()
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategory()
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/posts", name="posts")
+     */
+    public function posts()
+    {
+        return $this->render('admin/posts.html.twig');
+    }
+
+    /**
+     * @Route("/upload-post", name="upload_post")
+     */
+    public function uploadPost()
+    {
+        return $this->render('admin/upload_post.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users()
+    {
+        return $this->render('admin/users.html.twig');
+    }
 }
