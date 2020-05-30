e884c247afed5139dba291a8f8c1d2a7078ca5ee
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-05-30 02:39:32 +0100

    commit

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -11,10 +11,27 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-       $this->loadMainCategoriesData($manager);
+       $this->loadMainCategories($manager);
+       $this->loadSubCategories($manager, 'PHP', 1);
     }
 
-    private function loadMainCategoriesData($manager)
+    private function loadSubCategories($manager,  $category, $parent_id)
+    {
+        $parent = $manager->getRepository(Category::class)->find($parent_id);
+        $methodName = "get{$category}Data";
+
+        foreach ($this->getPHPData() as [$name]) {
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
+            $manager->persist($category);
+        }
+        
+
+        $manager->flush();
+    }
+
+    private function loadMainCategories($manager)
     {
         foreach ($this->getMainCategoriesData() as [$name]) {
             
@@ -32,12 +49,21 @@ class CategoryFixtures extends Fixture
         return [
             ['PHP', 1],
             ['JavaScript', 2],
-            ['TypeScript', 3],
-            ['GO', 4],
-            ['Java', 5],
-            ['Python',  6]
+            ['GO', 3],
+            ['Java', 4]
         ];
     }
+
+    private function getPHPData()
+    {
+        return [
+            ['Symfony', 5],
+            ['Laravel', 6],
+            ['PHP API', 7],
+            ['PHP UnitTests', 8]
+        ];
+    }
+
 
 
 
--- src/Migrations/Version20200530013822.php
+++ src/Migrations/Version20200530013822.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200529234742 extends AbstractMigration
+final class Version20200530013822 extends AbstractMigration
 {
     public function getDescription() : string
     {
