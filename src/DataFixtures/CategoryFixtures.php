80bb4664243b79f5117c2bc42f28644f523abb0a
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-05-30 03:23:14 +0100

    commit

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -12,25 +12,14 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
        $this->loadMainCategories($manager);
-       $this->loadSubCategories($manager, 'PHP', 1);
+       $this->loadPHP($manager);
+       $this->loadJavaScript($manager);
+       $this->loadGO($manager);
+       $this->loadJava($manager);
+       $this->loadNodeJS($manager);
     }
 
-    private function loadSubCategories($manager,  $category, $parent_id)
-    {
-        $parent = $manager->getRepository(Category::class)->find($parent_id);
-        $methodName = "get{$category}Data";
-
-        foreach ($this->getPHPData() as [$name]) {
-            $category = new Category();
-            $category->setName($name);
-            $category->setParent($parent);
-            $manager->persist($category);
-        }
-        
-
-        $manager->flush();
-    }
-
+    
     private function loadMainCategories($manager)
     {
         foreach ($this->getMainCategoriesData() as [$name]) {
@@ -43,6 +32,53 @@ class CategoryFixtures extends Fixture
 
         $manager->flush();
     }
+
+    private function loadPHP($manager)
+    {
+        $this->loadSubcategories($manager, 'PHP', 1);
+    }
+
+    private function loadJavaScript($manager)
+    {
+        $this->loadSubcategories($manager, 'JavaScript', 2);
+    }
+
+    private function loadGO($manager)
+    {
+        $this->loadSubcategories($manager, 'GO', 3);
+    }
+
+    private function loadJava($manager)
+    {
+        $this->loadSubcategories($manager, 'Java', 4);
+    }
+
+    private function loadNodeJS($manager)
+    {
+        $this->loadSubcategories($manager, 'NodeJS', 10);
+    }
+
+
+
+
+
+    private function loadSubcategories($manager,  $category, $parent_id)
+    {
+        $parent = $manager->getRepository(Category::class)->find($parent_id);
+        $methodName = "get{$category}Data";
+
+        foreach ($this->$methodName() as [$name]) 
+        {
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
+            $manager->persist($category);
+        }
+        
+
+        $manager->flush();
+    }
+
 
     private function getMainCategoriesData()
     {
@@ -64,8 +100,42 @@ class CategoryFixtures extends Fixture
         ];
     }
 
+    private function getJavaScriptData()
+    {
+        return [
+            ['VanillaJS', 9],
+            ['NodeJS', 10],
+            ['Deno', 11],
+            ['React', 12]
+        ];
+    }
+
+    private function getGOData()
+    {
+        return [
+            ['GOsubCategory1', 13],
+            ['GOsubCategory2', 14]
+        ];
+    }
+
+    private function getJavaData()
+    {
+        return [
+            ['JavaSubcategory1', 15],
+            ['JavaSubcategory2', 16],
+            ['JavaSubcategory3', 17]
+        ];
+    }
 
 
+    private function getNodeJSData()
+    {
+        return [
+            ['Express', 18],
+            ['mongoose', 19],
+            ['sequelize', 20]
+        ];
+    }
 
 
 
--- src/Migrations/Version20200530021946.php
+++ src/Migrations/Version20200530021946.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200530013822 extends AbstractMigration
+final class Version20200530021946 extends AbstractMigration
 {
     public function getDescription() : string
     {
