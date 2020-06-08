e4ed9511593ec4b35ecd9fc06cf0a954fe0456ae
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-06-08 04:48:07 +0100

    enhancing view

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -212,18 +212,42 @@ class FrontController extends AbstractController
 
     private function likePost($post)
     {  
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->addLikedPost($post);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush(); 
         return 'liked';
     }
     private function dislikePost($post)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->addDislikedPost($post);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush(); 
         return 'disliked';
     }
     private function undoLikePost($post)
     {  
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->removeLikedPost($post);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush(); 
         return 'undo liked';
     }
     private function undoDislikePost($post)
     {   
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->removeDislikedPost($post);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
         return 'undo disliked';
     }
 
