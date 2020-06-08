2be635ee45e21079a736c838e75ee1e2dc2b8474
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-06-08 17:12:57 +0100

    testUserDeleted

--- src/Migrations/Version20200608161110.php
+++ src/Migrations/Version20200608161110.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200608143509 extends AbstractMigration
+final class Version20200608161110 extends AbstractMigration
 {
     public function getDescription() : string
     {
